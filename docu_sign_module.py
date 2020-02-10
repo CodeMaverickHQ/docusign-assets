f9a619f52830645819a9d79aadac7ea994fd2cb7
Jeffrey Vos <jeff.vos@aol.com>
2020-02-10 10:16:23 +0200

    structural changes

--- docu_sign_module.py
+++ docu_sign_module.py
@@ -1,31 +1,34 @@
-import requests
-import cata_it
+''' starting point for docusign intergration '''
 import json
-#need to be moved to json
-docusignUrl='https://demo.docusign.net/restapi/v2/accounts/{accountID}/envelopes'
-data = userAgreementFile.data
+import requests
 
+
+DOCU_URL = 'https://demo.docusign.net/restapi/v2/accounts/{accountID}/envelopes'
+
+with open('user_agreement.json', 'r') as agree_data:
+    DATA = json.load(agree_data)
 with open('credsData.json', 'r') as credData:
-    auth2=json.load(credData)
+    AUTH = json.load(credData)
 
-def userAgreement(admin,user,asset):
-    querystring = {"api_password":"true"}
+
+def user_agreement(admin, user, asset):
+    ''' composing of the agreement '''
+    querystring = {"api_password": "true"}
     headers = {
         'X-DocuSign-Authentication': "",
         'Content-Type': "application/json",
         'Cache-Control': "no-cache",
-        }
-    headers['X-DocuSign-Authentication']=auth2['docu_user']
-    #admin user
-    data["recipients"]["signers"][1]["email"]=(admin['email'])
-    data["recipients"]["signers"][1]["name"]=(admin['name'])
-    #end-user
-    data["recipients"]["signers"][0]["email"]=(user['email'])
-    data["recipients"]["signers"][0]["name"]=(user['name'])
-    #asset
-    data["recipients"]["signers"][1]["tabs"]["textTabs"][0]["value"]=(asset)
-    data["recipients"]["signers"][0]["tabs"]["textTabs"][0]["value"]=(asset)
-    r = requests.request("POST", url=docusignUrl, data=json.dumps(data), headers=headers, params=querystring)
-    print(r.text)
-    return('done')
-
+    }
+    headers['X-DocuSign-Authentication'] = AUTH['docu_user']
+    # admin user
+    DATA["recipients"]["signers"][1]["email"] = (admin['email'])
+    DATA["recipients"]["signers"][1]["name"] = (admin['name'])
+    # end-user
+    DATA["recipients"]["signers"][0]["email"] = (user['email'])
+    DATA["recipients"]["signers"][0]["name"] = (user['name'])
+    # asset
+    DATA["recipients"]["signers"][1]["tabs"]["textTabs"][0]["value"] = (asset)
+    DATA["recipients"]["signers"][0]["tabs"]["textTabs"][0]["value"] = (asset)
+    req = requests.request("POST", url=DOCU_URL, data=json.dumps(
+        DATA), headers=headers, params=querystring)
+    return req.text
